fbec5cd29b56a8d890455f731a36924fdf411e74
Feng <bufeng.happy@163.com>
2016-07-23 13:17:08 +0800

    按钮点击事件

--- app/src/main/java/com/example/feng/snowfall/MainActivity.java
+++ app/src/main/java/com/example/feng/snowfall/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
